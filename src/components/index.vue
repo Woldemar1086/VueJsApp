4d4c206966c07a25775eea9be8c7f94c3d581d45
Woldemar1086 <khodyrev.v.a@gmail.com>
2017-09-15 14:20:34 +0200

    Routing for index

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,10 +1,8 @@
 <template>
-<div>
-  <p> 
-    Index
-  </p>
-  <router-link tag="a" to="vuejs"><h3>Welcome on my website</h3></router-link>
-</div>
+	<div class="index">
+		<img src="">
+		<router-link tag="a" to="vuejs"><h3>Welcome on my website</h3></router-link>
+	</div>
 </template>
 <script>
 
@@ -12,4 +10,24 @@
 
 <style>
 
+html, body{
+	height: 100%;
+	width: 100%;
+	padding: 0;
+	margin: 0;
+}
+
+.index{
+	background-image: url('../assets/darkrift-orig_full-min.jpg');
+	background-size: cover;
+	background-position: center, center;
+	width: 100%;
+	height: 100%;
+}
+.index a {
+	text-align: center;
+	font-size: 2em;
+	color: white;
+	text-decoration: none !important;
+}
 </style>
