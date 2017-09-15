6801702e3f5fc0d22802a4beee02e7322e29ad43
Woldemar1086 <khodyrev.v.a@gmail.com>
2017-09-15 16:21:09 +0200

    Deploy in Heroku

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import App from './app.vue'
+import app from './app.vue'
 import { routes } from './system/routes'
 
 Vue.use(VueRouter);
@@ -13,5 +13,5 @@ export const router = new VueRouter({
 new Vue({
   el: '#app',
   router,
-  render: h => h(App)
+  render: h => h(app)
 })
