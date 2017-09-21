cd0273f959b6f38532d1e2f7a889e821ef319d8d
Woldemar1086 <khodyrev.v.a@gmail.com>
2017-09-21 13:12:42 +0200

    Added animation for authentication form

--- src/components/authForm.vue
+++ src/components/authForm.vue
@@ -5,40 +5,84 @@
 				<h2>Welcome to new World!</h2>
 				<div class="authHeaderTabs">
 					<input class="signOrloginButton" type="radio" name="tab" id="signUp" checked/>
-					<label for="signUp">Sign up</label>
+					<label for="signUp" @click="showTab(1)">Sign up</label>
 					<input class="signOrloginButton" type="radio" name="tab" id="login"/>
-					<label for="login">Login</label>
+					<label for="login" @click="showTab(2)">Login</label>
 				</div>
 			</div>
-			<div class="authForm">
-				<button class="facebookBtn">Continue with Facebook</button>
-				<form>
-					<div class="fieldContainer">
-						<label class="labelFields" for="email">E-mail</label>
-						<input class="authFields" type="email" name="" id="email" required>
-					</div>
-					<div class="fieldContainer">
-						<label class="labelFields" for="password">Password</label>
-						<input class="authFields" type="password" name="" id="password" required>
-					</div>
-					<div class="fieldContainer">
-						<label class="labelFields" for="password">Repeat your password</label>
-						<input class="authFields" type="password" name="" id="password" required>
-					</div>
-				<!-- <button class="authSocButton"> some text here</button> -->
-				</form>
-				
-			</div>
-			<div class="authBtnContainer">
-				<button class="authBtn">Login</button>
-			</div>
-			
+			<transition :duration="{ enter: 200, leave: 100 }" name="component-fade">
+				<div class="authForm" v-show="showSignForm">
+					<button class="facebookBtn">Continue with Facebook</button>
+					<form>
+						<div class="fieldContainer">
+							<label class="labelFields" for="email">E-mail</label>
+							<input class="authFields" type="email" name="" id="email" required>
+						</div>
+						<div class="fieldContainer">
+							<label class="labelFields" for="password">Password</label>
+							<input class="authFields" type="password" name="" id="password" required>
+						</div>
+						<div class="fieldContainer">
+							<label class="labelFields" for="password">Repeat your password</label>
+							<input class="authFields" type="password" name="" id="passwordRepeat" required>
+						</div>
+						<div class="authBtnContainer">
+							<button class="authBtn">Sign up</button>
+						</div>
+					<!-- <button class="authSocButton"> some text here</button> -->
+					</form>
+					
+				</div>
+			</transition>
+			<transition :duration="{ enter: 200, leave: 100 }" name="component-fade">
+				<div class="authForm" v-show="showLogForm">
+					<form>
+						<div class="fieldContainer">
+							<label class="labelFields" for="email">E-mail</label>
+							<input class="authFields" type="email" name="" id="emaillog" required>
+						</div>
+						<div class="fieldContainer">
+							<label class="labelFields" for="password">Password</label>
+							<input class="authFields" type="password" name="" id="passwordlog" required>
+						</div>
+						<div class="authBtnContainer">
+							<button class="authBtn">Login</button>
+						</div>
+					<!-- <button class="authSocButton"> some text here</button> -->
+					</form>
+					
+				</div>
+			</transition>
 		</div>
 	</div>
 
 </template>
 
 <script>
+export default {
+	data(){
+		return {
+			showSignForm: true,
+			showLogForm: false
+		}
+	},
+	methods:{
+		showTab(num){
+			switch(num){
+				case 1:
+				this.showSignForm = true
+				this.showLogForm = false
+				break;
+				case 2:
+				this.showSignForm = false
+				this.showLogForm = true
+				break;
+				default:
+				break;
+			}
+		}
+	}
+}
 
 </script>
 
